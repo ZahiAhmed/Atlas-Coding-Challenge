32af6e5442352ab50a73238eac3ff6968b583960
ZahiAhmed <zahi.ahmed33@gmail.com>
2024-08-19 13:17:50 -0400

    coding challenge solutions

---
+++ soccer.rb
@@ -0,0 +1,40 @@
+def findMinDiff(input)
+    all_stats = []
+
+    #creating custom labels in case different datasets have different headings
+    labels = [:Team, :P, :W, :L, :D, :F, :A, :Points]
+    
+    File.readlines(input).each do |line|
+        parts = line.split
+        
+        next if parts.length < 2
+        labels = parts.map {|label| label.to_sym} if labels.empty?
+
+        #relevant data starts with a numerical value
+        next if parts[0].to_i < 1 
+        
+        #standings doesnt have a column name in data
+        standing = parts.shift.to_i
+        
+        stats = {}
+        
+        labels.each.with_index do |label, i| 
+            #bypass "-" between :F and :A columns
+            i+=1 if parts[i].to_i < 1 unless label == :Team 
+
+            stats[label] = parts[i]
+        end
+        
+        #create standing and difference key in data hash
+        stats[:Standing] = standing
+        stats[:Difference] = (stats[:F].to_i - stats[:A].to_i).abs
+        
+        all_stats << stats
+    end
+    
+    #sort data by difference returns first element
+    all_stats.sort_by{|stats| stats[:Difference]}[0]
+end
+
+#prints :Team of returned value
+p findMinDiff("soccer.dat")[:Team]
---
+++ w_data.rb
@@ -0,0 +1,30 @@
+def findMinTempSpread(input)
+    w_data = []
+    
+    #only first 3 columns are required
+    labels = [:Day, :Max, :Min] 
+    
+    File.readlines(input).each do |line|
+        parts = line.split
+        
+        #relevant data starts with a numerical value
+        next if parts[0].to_i < 1 
+        
+        d_data = {}
+        
+        labels.each.with_index do |label, i|
+            d_data[label] = parts[i]
+        end
+        
+        #create :TempSpread key for each data hash
+        d_data[:TempSpread] = d_data[:Max].to_i - d_data[:Min].to_i
+        
+        w_data << d_data
+    end
+    
+    #sort by :TempSpread and return first element
+    w_data.sort_by{|d_data| d_data[:TempSpread]}[0]
+end
+
+#prints :Day of returned value
+p findMinTempSpread("w_data.dat")[:Day]
